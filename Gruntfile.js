cbf0f963cb201eef1ac843e638c04b03f0b27ebe
Guoyang Wei <weiguoyang14@163.com>
2016-07-01 14:42:55 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt)
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            files:['static/less/main.less'],
+            tasks:['default']
+        },
+        uglify: {
+            build: {
+                expand: true,
+                cwd: 'static/js/',
+                src: 'main.js',
+                dest: 'static/js/',
+                ext: '.min.js',
+                extDot: 'last'
+            }
+        },
+        less: {
+            options: {
+                compress:true
+            },
+            build: {
+                expand: true,
+                cwd: 'static/less/',
+                src: ['*.less','!global.less','!mixins.less','!variables.less'],
+                dest: 'static/css/',
+                ext: '.min.css',
+                extDot: 'last'
+            }
+        },
+        htmlmin: {                                     // Task
+            dist: {                                      // Target
+                options: {                                 // Target options
+                    removeComments: true,  
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'index.html': 'dist/index.html',     // 'destination': 'source'
+                    'works.html': 'dist/works.html',
+                    'info.html': 'dist/info.html',
+                    'blogs.html': 'dist/blogs.html'
+                }
+            }
+        }
+    });
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify','less','htmlmin']);
+
+};
